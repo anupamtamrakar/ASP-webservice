cdcf00b1dee7b9b3235aeb0bd474b16f4e5e10cc
anupamtamrakar <anupam.tamrakar@aquasec.com>
2021-08-15 01:16:03 +0530

    ASP webservice project

---
+++ CS.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            this.BindGrid();
+        }
+    }
+
+    private void BindGrid()
+    {
+        CRUD_Service.ServiceCS service = new CRUD_Service.ServiceCS();
+        GridView1.DataSource = service.Get();
+        GridView1.DataBind();
+    }
+}
